808107878fedad71f306f25b7b38dd95f50865d0
SpaceEnergy <KiwiEnergy0@gmail.com>
2025-05-06 15:34:01 +0200

    feat: transparent logo

--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -26,7 +26,7 @@ const path = require('path');
 
   fs.mkdirSync('screenshots/grid', { recursive: true });
   const gameIds = [];
-  const elementIds = ['grid', 'p', 'hero'];
+  const elementIds = ['grid', 'p', 'hero', 'logo'];
 
   for (const file of files) {
     const appId = path.basename(path.dirname(file));
